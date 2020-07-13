a84f0614e4a5d892a09613c503de1105f1eeddc3
ym <mengyve.d@gmail.com>
2020-07-13 09:03:16 +0800

    task add

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 
 tasks = ["food", "barbor", "pizza"]
+
+class NewTaskForm(forms.Form):
+    task = form.CharField(label="New Task")
+
 def index(request):
     return render(request, 'tasks/index.html', {
         "tasks": tasks
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": "NewTaskform()"
+    })
